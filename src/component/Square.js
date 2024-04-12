7c0f6301e25373928bdfc605568fe9d0bc7095bf
caiofersousa <caiofersousa@hotmail.com>
2024-04-12 19:22:50 -0300

    feat: correcao dos imports

---
+++ src/component/Square.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Square extends React.Component {
+    render () {
+        return (
+            <button className="square">
+                {this.props.name}
+            </button>
+        );
+    }
+}
+
+export default Square;
